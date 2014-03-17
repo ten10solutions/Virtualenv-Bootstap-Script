26a12eff294e2339efa410cd7875fa04fd670e1e
Peter Russell <peter.russell@thetestpeople.com>
2014-03-17 11:06:54 +0000

    Import modified version of the bootstrap script.
    This script was used to quickly set up a room full of newbies with a
    desktop shortcut that would drop them into a commandprompt with a
    virtualenv activated.  It should only require powershell and Python 2.7
    installed (but not necessarily on the path).

---
+++ virtualenv-bootstrap.ps1
@@ -0,0 +1,22 @@
+$python = "c:\python27\python.exe";
+$pipfile = [io.path]::GetTempFileName();
+$url = "https://raw.github.com/pypa/pip/master/contrib/get-pip.py";
+$client = new-object System.Net.WebClient;
+$client.DownloadFile($url, $pipfile);
+Write-Debug "Downloaded Pip";
+& $python $pipfile --user --ignore-installed | write-host;
+
+& "$env:APPDATA\Python\Scripts\pip.exe" install --user --ignore-installed virtualenv | write-host;
+
+mkdir "$env:USERPROFILE\python-envs" -ErrorAction SilentlyContinue | Out-Null;
+
+$envpath = "$env:USERPROFILE\python-envs\automation"
+
+& "$env:APPDATA\Python\Scripts\virtualenv.exe" $envpath | write-host;
+
+
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Automation Command Prompt.lnk")
+$Shortcut.TargetPath = "$env:windir\system32\cmd.exe"
+$Shortcut.Arguments = "/k ""$envpath\Scripts\activate"""
+$Shortcut.Save()
